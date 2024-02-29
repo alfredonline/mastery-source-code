3199662cf0898942f3e70c00fa3a04af2e79c4ca
Alfie <afortd@yahoo.com>
2024-02-29 15:41:30 +0000

    add in basic styling setup

---
+++ constants/branding.ts
@@ -0,0 +1,12 @@
+export const brandName = "Mastery" as const;
+
+export const heroText = "Track how much time you spend on your goals" as const;
+
+export const heroSubtext = "Mastery helps you track your time and progress towards your goals." as const;
+
+export const ctaTextOptions = {
+    login: "Log in",
+    register: "Sign up",
+    getStarted: "Get started",
+    logOut: "Log out",
+} as const;
